81619af9342cc8b722085b9cdd1ea0f34e1559e3
Kares <CarlosEscribano113@gmail.com>
2026-02-25 20:09:50 +0100

    Autoheal

--- Assets/Scripts/Game/CorruptionGainManager.cs
+++ Assets/Scripts/Game/CorruptionGainManager.cs
@@ -1,28 +1,50 @@
 using System;
 using UnityEngine;
 using TypTyp;
+using System.Collections;
 
 [RequireComponent(typeof(Player))]
 public class CorruptionGainManager : MonoBehaviour
 {
     private Player player;
     public event Action<float> OnCorruptionGain;
+    private WaitForSeconds timeToAutoHeal;
+    private WaitForSeconds autoHealInterval;
     public float GainMultiplier { get; set; } = 1;
 
-    private void Start()
+    private void Awake()
     {
         player = GetComponent<Player>();
+        timeToAutoHeal = new WaitForSeconds(Settings.Instance.TimeToAutoHeal);
+        autoHealInterval = new WaitForSeconds(Settings.Instance.AutoHealInterval);
     }
 
     public void ProcessMistake()
     {
-        AddCorruption(Settings.Instance.MistakePenalizationPercentage / 100 * Settings.Instance.MaxCorruption);
+        AddCorruption(Settings.Instance.MistakePenalizationPercentage / 100 * 
+            Settings.Instance.MaxCorruption);
     }
 
     private void AddCorruption(float corruptionToAdd)
     {
+        if(corruptionToAdd > 0)
+        {
+            StopAllCoroutines();
+            StartCoroutine(AutoHealCoroutine());
+        }
         float currentCorruption = player.CurrentCorruption.Value;
-        currentCorruption = Mathf.Clamp(currentCorruption + corruptionToAdd, 0, Settings.Instance.MaxCorruption);
+        currentCorruption = Mathf.Clamp(currentCorruption + corruptionToAdd, 0, 
+            Settings.Instance.MaxCorruption);
         OnCorruptionGain?.Invoke(currentCorruption);
+    }
+
+    IEnumerator AutoHealCoroutine()
+    {
+        yield return timeToAutoHeal;
+        while (true)
+        {
+            AddCorruption(-Settings.Instance.AutoHealPercentage / 100 * Settings.Instance.MaxCorruption);
+            yield return autoHealInterval;
+        }
     }
 }
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -5,28 +5,31 @@ namespace TypTyp
     [CreateAssetMenu(menuName = "TypTyp/Settings")]
     public class Settings : ScriptableSingleton<Settings>
     {
+        [Header("TAGS & IDs")]
         [field: SerializeField] public int P1_ID { get; private set; } = 1;
         [field: SerializeField] public int P2_ID { get; private set; } = 2;
-
         [field: SerializeField] public string P1_tag { get; private set; } = "Player1";
         [field: SerializeField] public string P2_tag { get; private set; } = "Player2";
 
+        [Header("CORRUPTION RELATED")]
         //Maxima cantidad de corrupci�n para perder
         [field: SerializeField] public float MaxCorruption { get; private set; } = 100;
-
+        [field: SerializeField] public float AutoHealPercentage { get; private set; } = 0.2f;
+        [field: SerializeField] public float TimeToAutoHeal { get; private set; } = 3f;
+        [field: SerializeField] public float AutoHealInterval { get; private set; } = 0.5f;
         //Porcentaje de penalizaci�n de fallos. Ha de estar en el rango [0, 100]
         [field: SerializeField] public float MistakePenalizationPercentage { get; private set; } = 1.0f;
 
+        [Header("MANA RELATED")]
         //M�xima cantidad de man� 
         [field: SerializeField] public float MaxMana { get; private set; } = 100;
         [field: SerializeField] public int NumManaBars { get; private set; } = 5;
-
         //Ratio obtenci�n man� - progreso. Cuanto m�s alto, m�s man� se gana recitando el ritual
         [field: SerializeField] public float ManaGain { get; private set; } = 5;
 
+        [Header("RITUAL RELATED")]
         //Textos a completar para el ritual
         [field: SerializeField] public int MaxTextsProvided { get; private set; } = 10;
-
         [field: SerializeField] public bool DottedText { get; private set; } = true;
     }
 }
