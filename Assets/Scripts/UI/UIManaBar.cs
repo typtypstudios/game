07ba032a01d864daa6dbbc952fa5f6914090c8d7
Kares <CarlosEscribano113@gmail.com>
2026-02-26 00:10:36 +0100

    Fix colocación barras de maná
    La colocación de las barras de maná ahora no se hace con layout group porque daba problemas de inconsistencia de espaciado entre ellas. Ahora se colocan por código. El proceso sigue siendo automático

--- Assets/Scripts/UI/UIManaBar.cs
+++ Assets/Scripts/UI/UIManaBar.cs
@@ -7,7 +7,7 @@ public class UIManaBar : MonoBehaviour
     [Tooltip("Separación entre barras expresado en porcentaje del width de mana bar")]
     [Range(0f, 0.1f)][SerializeField] private float barSeparation = 0.01f;
     [SerializeField] private GameObject manaBarPrefab;
-    private List<UIBar> bars = new();
+    private readonly List<UIBar> bars = new();
     private float perBarPercentage;
     public float MaxValue { get; set; } = 1f;
 
@@ -28,15 +28,18 @@ public class UIManaBar : MonoBehaviour
         }
     }
 
-    private void CreateBars()
+    private void CreateBars() //Se colocan las barras manualmente y no en un layout group porque va mejor
     {
         float totalWidth = GetComponent<RectTransform>().sizeDelta.x;
         float totalSeparation = (Settings.Instance.NumManaBars - 1) * barSeparation * totalWidth;
         float barWidth = (totalWidth - totalSeparation) / Settings.Instance.NumManaBars;
+        float currentPos = barWidth / 2; //Posición de la primera barra
         for(int i = 0; i < Settings.Instance.NumManaBars; i++)
         {
             RectTransform bar = Instantiate(manaBarPrefab, transform).GetComponent<RectTransform>();
             bar.sizeDelta = new Vector2(barWidth, bar.sizeDelta.y);
+            bar.anchoredPosition = new Vector2(currentPos, 0);
+            currentPos += barWidth + barSeparation * totalWidth;
             UIBar uiBar = bar.GetComponent<UIBar>();
             if (i != 0) uiBar.PrevBar = bars[i - 1];
             bars.Add(uiBar);
