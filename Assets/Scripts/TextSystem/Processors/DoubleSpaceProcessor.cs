1118db3d07d1d213e550a0e8579effde67ef79b7
Kares <CarlosEscribano113@gmail.com>
2026-03-07 03:36:44 +0100

    Mejorado Separation Spell
    Ahora los espacios que se añaden son aleatorios entre un máximo y un mínimo

--- Assets/Scripts/TextSystem/Processors/DoubleSpaceProcessor.cs
+++ Assets/Scripts/TextSystem/Processors/DoubleSpaceProcessor.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using UnityEngine;
 
 namespace TypTyp.TextSystem
@@ -5,9 +6,22 @@ namespace TypTyp.TextSystem
     [CreateAssetMenu(fileName = "DoubleSpaceProcessor", menuName = "TypTyp/Text Processors/DoubleSpaceProcessor")]
     public class DoubleSpaceProcessor : ScriptableTextProcessor
     {
+        [SerializeField] int minSpacesAdded = 0;
+        [SerializeField] int maxSpacesAdded = 2;
+
         public override string ProcessText(string input)
         {
-            return input.Replace(" ", "  ");
+            StringBuilder output = new();
+            foreach(var c in input)
+            {
+                if (c == ' ')
+                {
+                    int spacesAdded = Random.Range(minSpacesAdded, maxSpacesAdded + 1) + 1; //El original se respeta con el +1
+                    output.Append(' ', spacesAdded);
+                }
+                else output.Append(c);
+            }
+            return output.ToString();
         }
     }
 }
