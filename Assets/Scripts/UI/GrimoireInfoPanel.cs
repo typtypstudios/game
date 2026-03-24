3bab27814d8496bdf1170a54c753a0a32507c2cf
Kares <CarlosEscribano113@gmail.com>
2026-03-24 01:29:26 +0100

    Info panel grimoire
    Añadido al panel de información detallada soporte para mostrar información sobre cartas. Tags para cartas y efectos:
    - <value> en efectos poner el valor específico de ese efecto en concreto (incremento de daño, aumento de maná...)
    - <duration> pone la duración del efecto
    - En cartas, poner entre comillas simples (') algo interpretará que es un efecto de estado, y lo coloreará
    - En cartas, poner <effectSelf_X> o <effectEnemy_X> rellenará con la descripción del modificador de estado en la posición X. Si se añade una c a la derecha de la X el punto final se elimina, permitiendo que concatene con lo siguiente

--- Assets/Scripts/UI/GrimoireInfoPanel.cs
+++ Assets/Scripts/UI/GrimoireInfoPanel.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,12 +11,14 @@ public class GrimoireInfoPanel : MonoBehaviour
     [SerializeField] private Color durationHighlightColor;
     [SerializeField] private Color positiveHighlightColor = Color.green;
     [SerializeField] private Color negativeHighlightColor = Color.red;
+    [SerializeField] private Color effectHighlightColor = Color.purple;
     private string durationColor;
     private string positiveColor;
     private string negativeColor;
+    private string effectColor;
 
 
-    [SerializeField] private StatusEffectDefinition startInfo;//CAMBIAR LUEGO PARA QUE COJA AUTOMÁTICAMENTE PRIMERA CARTA
+    [SerializeField] private CardDefinition startInfo;//CAMBIAR LUEGO PARA QUE COJA AUTOMÁTICAMENTE PRIMERA CARTA
 
 
     private void Start()
@@ -23,6 +26,7 @@ public class GrimoireInfoPanel : MonoBehaviour
         durationColor = Utils.ColorToTag(durationHighlightColor);
         positiveColor = Utils.ColorToTag(positiveHighlightColor);
         negativeColor = Utils.ColorToTag(negativeHighlightColor);
+        effectColor = Utils.ColorToTag(effectHighlightColor);
         SetInfo(startInfo);
     }
 
@@ -30,7 +34,38 @@ public class GrimoireInfoPanel : MonoBehaviour
     {
         image.sprite = card.CardImage;
         nameText.text = card.CardName;
-        infoText.text = card.Description;
+        infoText.text = FillCardInfo(card);
+    }
+
+    private string FillCardInfo(CardDefinition card)
+    {
+        string desc = card.Description;
+        MatchCollection matches = Regex.Matches(desc, @"'([^']*)'");
+        desc = desc.Replace("'", "");
+        foreach (Match m in matches) 
+        {
+            string effectName = m.Groups[1].Value;
+            desc = desc.Replace(effectName, effectColor + effectName + "</color>");
+        }
+        matches = Regex.Matches(desc, @"<effectSelf_([a-zA-Z0-9]+)>");
+        foreach (Match m in matches)
+        {
+            int idx = int.Parse(m.Groups[1].Value[0].ToString());
+            string effectDesc = FillStatusInfo(card.Spell.OnSelfEffects[idx]);
+            if (m.Groups[1].Value.ToLower().Contains("c")) 
+                effectDesc = effectDesc.Remove(effectDesc.Length - 1);
+            desc = desc.Replace(m.Groups[0].Value, effectDesc);
+        }
+        matches = Regex.Matches(desc, @"<effectEnemy_([a-zA-Z0-9]+)>");
+        foreach (Match m in matches)
+        {
+            int idx = int.Parse(m.Groups[1].Value[0].ToString());
+            string effectDesc = FillStatusInfo(card.Spell.OnEnemyEffects[idx]);
+            if (m.Groups[1].Value.ToLower().Contains("c")) 
+                effectDesc = effectDesc.Remove(effectDesc.Length - 1);
+            desc = desc.Replace(m.Groups[0].Value, effectDesc);
+        }
+        return desc;
     }
 
     public void SetInfo(StatusEffectDefinition effect)
@@ -44,11 +79,12 @@ public class GrimoireInfoPanel : MonoBehaviour
     private string FillStatusInfo(StatusEffectDefinition effect)
     {
         string desc = effect.Description;
+        desc = desc.Replace("seconds", "seconds</color>").Replace("lines", "lines</color>");
         if (effect.DurationValue == 1) desc = desc.Replace("seconds", "second").Replace("lines", "line");
-        desc = desc.Replace("<duration>", durationColor + effect.DurationValue + "</color>");
+        desc = desc.Replace("<duration>", durationColor + effect.DurationValue);
         string polarityColor = effect.EffectPolarityType == EffectPolarityType.Bad ? 
             negativeColor : positiveColor;
-        desc = desc.Replace("<default>", polarityColor + effect.GetDefaultValue() + "</color>");
+        desc = desc.Replace("<value>", polarityColor + effect.GetDefaultValue() + "</color>");
         return desc;
     }
 }
