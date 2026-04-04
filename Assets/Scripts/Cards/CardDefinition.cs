a79c3e22a084e38be5c129a7ff969fd9d4b2e8af
J. Amaya <alejor016@gmail.com>
2026-04-04 22:30:19 +0200

    Cult Definition and Register

--- Assets/Scripts/Cards/CardDefinition.cs
+++ Assets/Scripts/Cards/CardDefinition.cs
@@ -1,8 +1,10 @@
+using TypTyp.Cults;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CardDefinition", menuName = "TypTyp/Cards/CardDefinition")]
 public class CardDefinition : ADefinition
 {
+    [field: SerializeField] public CultDefinition Cult { get; private set; }
     [field: SerializeField] public SpellDefinition Spell { get; private set; }
     [field: SerializeField] public int ManaCost { get; private set; } = 1;
 }
---
+++ Assets/Scripts/Cults/CultDefinition.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace TypTyp.Cults
+{
+    [CreateAssetMenu(fileName = "CultDefinition", menuName = "TypTyp/Cults/CultDefinition")]
+    public class CultDefinition : ScriptableObject
+    {
+        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public string[] RankNames { get; private set; }
+
+        public IEnumerable<CardDefinition> GetCards()
+        {
+            return CardRegister.Instance.RegisteredItems.Where(x => x.Cult == this);
+        }
+
+        public string GetRank(int rank)
+        {
+            var clampedRank = Math.Clamp(rank, 0, RankNames.Length);
+            if(clampedRank != rank)
+            {
+                Debug.LogWarning("The rank is outside bounds. Rank used is clamped");
+            }
+            return RankNames[clampedRank];
+        }
+    }
+}
