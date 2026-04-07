729c0394fdef8b55eb74f0d146b1372d75e383ed
Kares <CarlosEscribano113@gmail.com>
2026-04-07 06:41:15 +0200

    Efecto navegación
    Algo parece estar raro en el color de la transición, mañana lo miro.

--- Assets/Scripts/UI/Navigation/NavigationController.cs
+++ Assets/Scripts/UI/Navigation/NavigationController.cs
@@ -1,17 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class NavigationController : MonoBehaviour
 {
     [SerializeField] private NavigationEntry[] entries;
+    [SerializeField] private Material transitionMat;
+    [SerializeField] private float transitionSpeed = 1;
+    [SerializeField] private RenderTexture transitionTexture;
     private readonly Dictionary<Screens, Canvas> screenDictionary = new();
     private readonly Stack<Screens> screenStack = new();
     private Screens currentScreen;
+    private Camera uiCam;
+    private float Dissolve
+    {
+        get { return transitionMat.GetFloat("_Dissolve"); }
+        set { transitionMat.SetFloat("_Dissolve", Mathf.Clamp01(value)); }
+    }
 
     private void Awake()
     {
+        uiCam = GameObject.FindGameObjectWithTag("UICam").GetComponent<Camera>();
         int enabledCount = 0;
+        Canvas initCanvas = null;
         foreach(var entry in entries)
         {
             screenDictionary[entry.screen] = entry.canvas;
@@ -19,9 +32,24 @@ public class NavigationController : MonoBehaviour
             {
                 enabledCount++;
                 currentScreen = entry.screen;
+                initCanvas = entry.canvas;
             }
+            if (!entry.canvas.TryGetComponent(out CanvasGroup canvasGroup))
+                canvasGroup = entry.canvas.AddComponent<CanvasGroup>();
+            canvasGroup.blocksRaycasts = false;
         }
         if (enabledCount != 1) Debug.LogError("Error: solo un canvas debe estar activo al inicio.");
+        InitRT();
+        Dissolve = 1;
+        StartCoroutine(TransitionCoroutine(initCanvas, initCanvas));
+    }
+
+    private void InitRT()
+    {
+        transitionTexture.Release();
+        transitionTexture.width = Screen.width;
+        transitionTexture.height = Screen.height;
+        transitionTexture.Create();
     }
 
     public void GoTo(Screens screen)
@@ -43,11 +71,40 @@ public class NavigationController : MonoBehaviour
 
     private void NavigateToScreen(Screens screen, bool isGoingBack)
     {
-        foreach (var ctxReceiver in screenDictionary[screen].GetComponentsInChildren<INavigationCtxReceiver>(true))
-            ctxReceiver.ReceiveContext(currentScreen, isGoingBack);
-        screenDictionary[currentScreen].enabled = false;
+        Canvas originCanvas = screenDictionary[currentScreen];
+        Canvas destinationCanvas = screenDictionary[screen];
+        INavigationCtxReceiver[] receivers = 
+            destinationCanvas.GetComponentsInChildren<INavigationCtxReceiver>(true);
+        foreach (var receiver in receivers)
+            receiver.ReceiveContext(currentScreen, isGoingBack);
         currentScreen = screen;
-        screenDictionary[currentScreen].enabled = true;
+        StartCoroutine(TransitionCoroutine(originCanvas, destinationCanvas));
+    }
+
+    private IEnumerator TransitionCoroutine(Canvas origin, Canvas dest)
+    {
+        Camera.main.cullingMask &= ~(1 << LayerMask.NameToLayer("UI"));
+        uiCam.cullingMask |= (1 << LayerMask.NameToLayer("UI"));
+        origin.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        float dissolveValue = Dissolve; //Para no hacer gets constantes
+        while (dissolveValue < 1)
+        {
+            dissolveValue += transitionSpeed * Time.deltaTime;
+            Dissolve = dissolveValue;
+            yield return null;
+        }
+        origin.enabled = false;
+        dest.enabled = true;
+        dissolveValue = 1;
+        while (dissolveValue > 0)
+        {
+            dissolveValue -= transitionSpeed * Time.deltaTime;
+            Dissolve = dissolveValue;
+            yield return null;
+        }
+        dest.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        Camera.main.cullingMask |= (1 << LayerMask.NameToLayer("UI"));
+        uiCam.cullingMask &= ~(1 << LayerMask.NameToLayer("UI"));
     }
 }
 
