2b125eab49a567d86ad608a628e9647c06757e6c
Kares <CarlosEscribano113@gmail.com>
2026-04-12 22:19:42 +0200

    Builder displayers info panel desplazado

--- Assets/Scripts/Cards/BuilderDisplayer.cs
+++ Assets/Scripts/Cards/BuilderDisplayer.cs
@@ -1,7 +1,6 @@
 using System;
 using TMPro;
 using UnityEngine;
-
 public class BuilderDisplayer : InfoDisplayer
 {
     [SerializeField] private TextMeshProUGUI description;
@@ -9,38 +8,27 @@ public class BuilderDisplayer : InfoDisplayer
     [SerializeField] private BuilderDisplayerBlocker blocker;
     public static event Action<BuilderDisplayer> OnCardChosen;
     public CardDefinition Card => Definition as CardDefinition;
-
     private RectTransform infoPanelRect;
-    private Transform infoPanelOriginalParent;
-    private int infoPanelOriginalSiblingIndex;
-    private Vector3 infoPanelOriginalLocalPosition;
     private Canvas rootCanvas;
-    private float infoPanelOffsetX;
-    private float infoPanelOffsetY;
-
+    private float infoPanelOffsetYRatio;
     private void Start()
     {
         infoPanelRect = infoPanel != null ? infoPanel.transform as RectTransform : null;
         if (infoPanelRect != null)
         {
-            infoPanelOriginalParent = infoPanelRect.parent;
-            infoPanelOriginalSiblingIndex = infoPanelRect.GetSiblingIndex();
-            infoPanelOriginalLocalPosition = infoPanelRect.localPosition;
-            infoPanelOffsetX = Mathf.Abs(infoPanelOriginalLocalPosition.x);
-            infoPanelOffsetY = infoPanelOriginalLocalPosition.y;
+            RectTransform parentRect = infoPanelRect.parent as RectTransform;
+            if (parentRect != null)
+                infoPanelOffsetYRatio = Mathf.Abs(infoPanelRect.localPosition.y) / parentRect.rect.height;
         }
-
         rootCanvas = GetComponentInParent<Canvas>()?.rootCanvas;
         infoPanel.gameObject.SetActive(false);
     }
-
     public override void SetInfo(ADefinition definition)
     {
         base.SetInfo(definition);
         description.text = definition.Description;
         blocker.CheckBlock(definition as CardDefinition);
     }
-
     public override void Highlight(bool highlight)
     {
         base.Highlight(highlight);
@@ -48,55 +36,16 @@ public class BuilderDisplayer : InfoDisplayer
         if (highlight)
             transform.parent.SetAsLastSibling();
     }
-
     private void EnableInfoPanel(bool enable)
     {
         infoPanel.gameObject.SetActive(enable);
-        if (!enable)
-        {
-            RestoreInfoPanelParent();
-            return;
-        }
-
+        if (!enable) return;
         infoPanel.SetInfo(Card);
         Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, transform.position);
-        float direction = screenPos.x > Screen.width * 0.5f ? -1f : 1f;
-
-        if (infoPanelRect != null && rootCanvas != null)
-        {
-            RectTransform rootRect = rootCanvas.transform as RectTransform;
-            Camera uiCamera = rootCanvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : rootCanvas.worldCamera;
-
-            infoPanelRect.SetParent(rootRect, false);
-            infoPanelRect.SetAsLastSibling();
-
-            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rootRect, screenPos, uiCamera, out Vector2 localPoint))
-            {
-                infoPanelRect.anchoredPosition = new Vector2(localPoint.x + infoPanelOffsetX * direction, localPoint.y + infoPanelOffsetY);
-            }
-        }
-        else
-        {
-            float newX = Mathf.Abs(infoPanel.transform.localPosition.x) * direction;
-            infoPanel.transform.localPosition = new(newX, infoPanel.transform.localPosition.y, infoPanel.transform.localPosition.z);
-        }
+        float direction = screenPos.y > Screen.height * 0.5f ? -1f : 1f;
+        RectTransform parentRect = infoPanelRect.parent as RectTransform;
+        float offsetY = parentRect != null ? infoPanelOffsetYRatio * parentRect.rect.height : Mathf.Abs(infoPanelRect.localPosition.y);
+        infoPanelRect.localPosition = new Vector3(infoPanelRect.localPosition.x, offsetY * direction, infoPanelRect.localPosition.z);
     }
-
-    private void RestoreInfoPanelParent()
-    {
-        if (infoPanelRect == null || infoPanelOriginalParent == null)
-        {
-            return;
-        }
-
-        if (infoPanelRect.parent != infoPanelOriginalParent)
-        {
-            infoPanelRect.SetParent(infoPanelOriginalParent, false);
-            int siblingIndex = Mathf.Clamp(infoPanelOriginalSiblingIndex, 0, infoPanelOriginalParent.childCount - 1);
-            infoPanelRect.SetSiblingIndex(siblingIndex);
-            infoPanelRect.localPosition = infoPanelOriginalLocalPosition;
-        }
-    }
-
     public void OnButtonClicked() => OnCardChosen?.Invoke(this);
 }
