9c13a329aea3948574860e8a0b294583d0269a5c
Kares <CarlosEscribano113@gmail.com>
2026-05-06 03:21:42 +0200

    LoreMenu text system

--- Assets/Scripts/Lore/LoreMenu.cs
+++ Assets/Scripts/Lore/LoreMenu.cs
@@ -1,39 +1,88 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Canvas))]
-public class LoreMenu : MonoBehaviour, INavigationCtxReceiver, INavigationLeaveReceiver
+public class LoreMenu : AInputListener, INavigationCtxReceiver, INavigationLeaveReceiver
 {
-    [SerializeField] private GameObject skullEyes;
-    private CameraNavigation cameraNavigation;
-    private VignetteController vignette;
-    private bool onFocus = false;
+    [SerializeField] private TMP_Text tmp;
+    [TextArea][SerializeField] private string[] texts;
+    [Min(0)][SerializeField] private float textAppearSpeed = 1;
+    [SerializeField] private InputActionReference clickAction;
+    private int textIdx = 0;
+    private int charIdx = 0;
+    private string currentText;
+    private bool isFocused = false;
+    private const float CHAR_APPEAR_INTERVAL = 0.1f;
 
     private void Awake()
     {
-        cameraNavigation = FindFirstObjectByType<CameraNavigation>();
-        if (!cameraNavigation) Debug.LogError("Error: no se encuentra el sistema de navegación de cámara");
-        vignette = FindFirstObjectByType<VignetteController>();
-        if (!vignette) Debug.LogWarning("Aviso: no se encuentra la viñeta");
-        CanvasTransitionManager.OnDissolved += ToggleEyes;
-        ToggleEyes();
+        clickAction.action.performed += ProcessInput;
     }
 
-    private void OnDestroy() => CanvasTransitionManager.OnDissolved -= ToggleEyes;
-
-    private void ToggleEyes()
+    private void OnDestroy()
     {
-        skullEyes.SetActive(onFocus);
+        clickAction.action.performed -= ProcessInput;
+        CanvasTransitionManager.OnTransitionFinished -= DisplayText;
+    }
+
+    protected override void ProcessInput(char _) => OnInteraction();
+
+    private void ProcessInput(InputAction.CallbackContext _) => OnInteraction();
+
+    private void OnInteraction()
+    {
+        if (!isFocused) return;
+        StopAllCoroutines();
+        if (charIdx < currentText.Length)
+        {
+            charIdx = currentText.Length;
+            UpdateView();
+        }
+        else
+        {
+            textIdx++;
+            DisplayText();
+        }
     }
 
     public void ReceiveContext(Screens prevScreen, bool isGoingBack, GameObject sender = null)
     {
-        onFocus = true;
-        vignette.FadeIn(cameraNavigation.InterpolationTime);
+        textIdx = 0;
+        charIdx = 0;
+        CanvasTransitionManager.OnTransitionFinished += DisplayText;
+        isFocused = true;
     }
-        
+
     public void OnLeave()
     {
-        onFocus = false;
-        vignette.FadeOut(cameraNavigation.InterpolationTime);
+        CanvasTransitionManager.OnTransitionFinished -= DisplayText;
+        StopAllCoroutines();
+        isFocused = false;
+        tmp.text = string.Empty;
+    }
+
+    private void DisplayText()
+    {
+        currentText = textIdx < texts.Length ? texts[textIdx] : "...";
+        charIdx = 0;
+        StopAllCoroutines();
+        StartCoroutine(DisplayTextCoroutine());
+    }
+
+    private void UpdateView()
+    {
+        tmp.text = currentText[..charIdx] + "<alpha=#00>" + currentText[charIdx..];
+    }
+
+    IEnumerator DisplayTextCoroutine()
+    {
+        while(charIdx <= currentText.Length)
+        {
+            yield return new WaitForSeconds(CHAR_APPEAR_INTERVAL * textAppearSpeed);
+            UpdateView();
+            charIdx++;
+        }
     }
 }
---
+++ Assets/Scripts/Lore/LoreMenuVisualManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+[RequireComponent(typeof(LoreMenu))]
+public class LoreMenuVisualManager : MonoBehaviour, INavigationCtxReceiver, INavigationLeaveReceiver
+{
+    [SerializeField] private GameObject skullEyes;
+    private CameraNavigation cameraNavigation;
+    private VignetteController vignette;
+    private bool onFocus = false;
+
+    private void Awake()
+    {
+        cameraNavigation = FindFirstObjectByType<CameraNavigation>();
+        if (!cameraNavigation) Debug.LogError("Error: no se encuentra el sistema de navegación de cámara");
+        vignette = FindFirstObjectByType<VignetteController>();
+        if (!vignette) Debug.LogWarning("Aviso: no se encuentra la viñeta");
+        CanvasTransitionManager.OnDissolved += ToggleEyes;
+        ToggleEyes();
+    }
+
+    private void OnDestroy() => CanvasTransitionManager.OnDissolved -= ToggleEyes;
+
+    private void ToggleEyes()
+    {
+        skullEyes.SetActive(onFocus);
+    }
+
+    public void ReceiveContext(Screens prevScreen, bool isGoingBack, GameObject sender = null)
+    {
+        onFocus = true;
+        vignette.FadeIn(cameraNavigation.InterpolationTime);
+    }
+        
+    public void OnLeave()
+    {
+        onFocus = false;
+        vignette.FadeOut(cameraNavigation.InterpolationTime);
+    }
+}
