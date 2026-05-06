d347aef8259af56c4a57eef99913f48e17dd9f5f
Kares <CarlosEscribano113@gmail.com>
2026-05-06 21:42:41 +0200

    Navigation object grimoire

--- Assets/Scripts/UI/NavigationObject.cs
+++ Assets/Scripts/UI/NavigationObject.cs
@@ -7,14 +7,23 @@ public class NavigationObject : MonoBehaviour, IPointerClickHandler
     [SerializeField] private Screens destination;
     [SerializeField] private float minInteractionDistance = 20;
     private NavigationController controller;
+    private bool isFirstScreenLoaded = false;
 
     private void Awake()
     {
         controller = FindFirstObjectByType<NavigationController>();
+        CanvasTransitionManager.OnTransitionFinished += OnFirstSceneLoaded;
+    }
+
+    private void OnFirstSceneLoaded()
+    {
+        isFirstScreenLoaded = true;
+        CanvasTransitionManager.OnTransitionFinished -= OnFirstSceneLoaded;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        if (!isFirstScreenLoaded) return;
         if (Vector3.Distance(transform.position, Camera.main.transform.position) < minInteractionDistance) return;
         controller.GoTo(destination, this.gameObject);
     }
