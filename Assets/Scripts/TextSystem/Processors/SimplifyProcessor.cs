505163062ae9961ba2faa7059c6c8edd139080f7
Kares <CarlosEscribano113@gmail.com>
2026-05-10 01:51:25 +0200

    Cambios easy mode

--- Assets/Scripts/TextSystem/Processors/SimplifyProcessor.cs
+++ Assets/Scripts/TextSystem/Processors/SimplifyProcessor.cs
@@ -6,13 +6,14 @@ namespace TypTyp.TextSystem
     [CreateAssetMenu(fileName = "SimplifyProcessor", menuName = "TypTyp/Text Processors/SimplifyProcessor")]
     public class SimplifyProcessor : ScriptableTextProcessor
     {
+        [SerializeField] private string charsToRemove = ".!?:,";
         public override void ProcessText(StringBuilder builder, TextProcessContext context)
         {
             int writeIndex = 0;
             for (int readIndex = 0; readIndex < builder.Length; readIndex++)
             {
                 char c = builder[readIndex];
-                if (c == '.' || c == ',')
+                if (charsToRemove.Contains(c))
                     continue;
 
                 builder[writeIndex++] = char.ToLower(c);
