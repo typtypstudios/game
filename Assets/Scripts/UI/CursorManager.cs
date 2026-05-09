dff893752654b8ea1ae2a4ec9b1d952c86428473
Kares <CarlosEscribano113@gmail.com>
2026-05-09 17:52:48 +0200

    Comportamiento ratón mejorado

--- Assets/Scripts/UI/CursorManager.cs
+++ Assets/Scripts/UI/CursorManager.cs
@@ -9,22 +9,18 @@ using UnityEngine.UI;
 [NoAutoCreate]
 public class CursorManager : Singleton<CursorManager>
 {
-    [SerializeField] private Image defaultImage;
     [SerializeField] private Image hoverImage;
-    [SerializeField] private RectTransform parentRT;
-    private Canvas cursorCanvas;
+    private RectTransform hoverRT;
+    private Canvas hoverCanvas;
     private Camera cam;
 
     protected override void Awake()
     {
         base.Awake();
-        cursorCanvas = GetComponentInChildren<Canvas>();
-        Cursor.visible = false;
+        hoverCanvas = GetComponentInChildren<Canvas>();
+        hoverRT = hoverImage.GetComponent<RectTransform>();
         AssignCamera();
         SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-    private void Start()
-    {
         MatchManager.OnCountdownStarted += HideCursor;
         MatchManager.OnMatchEnded += ShowCursor;
     }
@@ -32,7 +28,6 @@ public class CursorManager : Singleton<CursorManager>
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         AssignCamera();
-        StartCoroutine(RestoreCanvasAfterFrame());
     }
 
     private void OnDestroy()
@@ -50,16 +45,16 @@ public class CursorManager : Singleton<CursorManager>
 
     private void Update()
     {
-        if (!cursorCanvas.enabled || cam == null)
+        if (!hoverCanvas.enabled || cam == null)
             return;
-        parentRT.position = Mouse.current.position.ReadValue();
+        hoverRT.position = Mouse.current.position.ReadValue();
         if (NavigationController.Navigating)
         {
             UpdateState(false);
             return;
         }
         //Objetos 3D:
-        if(Physics.Raycast(cam.ScreenPointToRay(parentRT.position), out RaycastHit hit))
+        if(Physics.Raycast(cam.ScreenPointToRay(hoverRT.position), out RaycastHit hit))
         {
             if (hit.transform.TryGetComponent(out ICursorHoverTarget target) &&
                 target.CanCauseHover())
@@ -71,7 +66,7 @@ public class CursorManager : Singleton<CursorManager>
         //Objetos interfaz:
         List<RaycastResult> results = new();
         EventSystem.current.RaycastAll(new PointerEventData(EventSystem.current)
-            { position = parentRT.position }, results);
+            { position = hoverRT.position }, results);
         foreach (var result in results)
         {
             Selectable s = result.gameObject.GetComponentInParent<Selectable>();
@@ -86,24 +81,18 @@ public class CursorManager : Singleton<CursorManager>
 
     private void UpdateState(bool hover)
     {
-        defaultImage.enabled = !hover;
+        Cursor.visible = !hover;
         hoverImage.enabled = hover;
     }
 
     private void HideCursor()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        cursorCanvas.enabled = false;
+        hoverCanvas.enabled = false;
     }
     private void ShowCursor()
     {
         Cursor.lockState = CursorLockMode.None;
-        cursorCanvas.enabled = true;
-    }
-
-    IEnumerator RestoreCanvasAfterFrame()
-    {
-        yield return null;
-        Canvas.ForceUpdateCanvases();
+        hoverCanvas.enabled = true;
     }
 }
