8539a1d7fc0dc1b577a0782c09932b0c6f4c56ea
Kares <CarlosEscribano113@gmail.com>
2026-05-10 04:00:15 +0200

    Mejora visual fill ritual bar
    Ahora el círculo es lo último que se llena, pues es lo que más fácil se ve de reojo e ir llenándolo al principio daba la sensación de que el ritual estaba por terminar. Además, ahora tiene mayor peso, lo que hace que se llene más lentamente que el resto

--- Assets/Scripts/UI/UIBarGroup.cs
+++ Assets/Scripts/UI/UIBarGroup.cs
@@ -1,26 +1,53 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class UIBarGroup : MonoBehaviour
 {
+    [Header("Manual creation:")]
+    [SerializeField] private WeightedBar[] weightedBars;
     protected List<IFillableBar> bars = new();
     protected float perBarPercentage;
     public float MaxValue { get; set; } = 1f;
+    private bool UsingWeights => weightedBars.Length > 0;
 
     void Start()
     {
+        if (UsingWeights) NormalizeWeights();
+        else InitDefault();
+    }
+
+    private void NormalizeWeights()
+    {
+        float sum = 0;
+        foreach (var bar in weightedBars)
+            sum += bar.weight;
+        foreach (var bar in weightedBars)
+            bar.weight /= sum;
+    }
+
+    private void InitDefault()
+    {
         if (bars.Count == 0) //A lo mejor se ha creado en un componente hijo y ya está el array completo
             bars = GetComponentsInChildren<IFillableBar>(true).
-                Where(b => { var c = b as Component; return c != null && 
-                    c.transform.parent != null && 
-                    (c.transform.GetComponent<UIBar_Multiple>() != null || 
-                    c.transform.parent.GetComponentInParent<UIBar_Multiple>() == null); })
+                Where(b => {
+                    var c = b as Component; return c != null &&
+                    c.transform.parent != null &&
+                    (c.transform.GetComponent<UIBar_Multiple>() != null ||
+                    c.transform.parent.GetComponentInParent<UIBar_Multiple>() == null);
+                })
                 .ToList();
         perBarPercentage = 1f / bars.Count;
     }
 
     public virtual void UpdateValue(float oldValue, float newValue)
+    {
+        if (!UsingWeights) DefaultUpdate(newValue);
+        else WeightedUpdate(newValue);
+    }
+
+    private void DefaultUpdate(float newValue)
     {
         float normalizedValue = newValue / MaxValue;
         foreach (IFillableBar bar in bars)
@@ -30,4 +57,22 @@ public class UIBarGroup : MonoBehaviour
             normalizedValue = Mathf.Clamp01(normalizedValue - perBarPercentage);
         }
     }
+
+    private void WeightedUpdate(float newValue)
+    {
+        float normalizedValue = newValue / MaxValue;
+        foreach (var wbar in weightedBars)
+        {
+            float barValue = normalizedValue >= wbar.weight ? 1.0f : normalizedValue / wbar.weight;
+            wbar.bar.UpdateValue(0, barValue);
+            normalizedValue = Mathf.Clamp01(normalizedValue - wbar.weight);
+        }
+    }
+}
+
+[Serializable]
+public class WeightedBar
+{
+    public UIBar bar;
+    [Range(0, 1)] public float weight = 1;
 }
